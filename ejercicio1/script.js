40c5a55ae8ab74f96b38ad3ec6014047feb4fabf
cristiananes <cristian-anes@hotmail.es>
2023-09-28 14:26:50 +0200

    Ejercicio 1 terminado con los apartados en funciones

--- ejercicio1/script.js
+++ ejercicio1/script.js
@@ -1,22 +1,40 @@
-var entero = prompt("introduce un numero entero");
+var entero =parseInt( prompt("introduce un numero entero"));
+
 //comprobar si es un numero
-if (!isNaN(entero)) {
-    console.log('es un número');
-} else {
-    alert('no es un número');}
+function esNumero (entero){
+    if (!isNaN(entero)) {
+        return  console.log('es un número');
+    } else {
+        return alert('no es un número');}
+}
+esNumero(entero);
+
 //comprobar si es entero
 
+function esEntero (entero){
+    if (parseInt(Number(entero)) == entero){
+        console.log("es un entero");
+        
+    } else {alert("no es un entero");}
+}
+esEntero(entero);
 
-if (parseInt(Number(entero)) == entero){
-    console.log("es un entero");
-    
-} else {alert("no es un entero")}
 
+//ahora vamos a comprobar si el número es positivo
+function esPositivo (entero){
+    if (entero <0){
+        console.log("el numero es negativo");
+    } else if (entero == 0){
+        console.log("el número es 0");
+    }  else {console.log("es un numeor positivo")}
+}
+esPositivo(entero);
+//hacemos el sumatorio
 function sumarNumeros(entero) {
     var suma = 0;
-    for (var i = 0; i < entero.length; i++) {
-        suma += entero[i];
+    for (var i = 0; i < entero; i++) {
+        suma = entero[i] + suma;
     }
     return suma;
 }
-sumarNumeros();
+console.log(sumarNumeros(entero));
